d7232acc9b9e3c5af4fe87c18818b0db94c89d9f
kristenmills <kristen@kristen-mills.com>
2014-04-24 23:40:14 -0400

    Tree works.

--- fractals/fractals.js
+++ fractals/fractals.js
@@ -1,4 +1,6 @@
+
 var Fractal = {
+  size: 900,
   tree: {
 
     generateNext: function(tree, theta) {
@@ -31,14 +33,39 @@ var Fractal = {
       }
     },
 
-    buildTree: function(depth, theta, tree){
+    buildTree: function(depth, theta, tree) {
       if(depth === 0){
         return tree;
       }
-      generateNext(tree, theta);
-      buildTree(depth-1, theta, tree->left);
-      buildTree(depth-1, theta, tree->right);
+      this.generateNext(tree, theta);
+      this.buildTree(depth-1, theta, tree.left);
+      this.buildTree(depth-1, theta, tree.right);
       return tree;
+    },
+
+    drawTree: function(two, tree){
+      
+      if(tree.left === null){
+        return;
+      }
+      var line1 = two.makeLine(Fractal.size - tree.x, Fractal.size - tree.y, Fractal.size - tree.left.x, Fractal.size - tree.left.y);
+      var line2 = two.makeLine(Fractal.size - tree.x, Fractal.size - tree.y, Fractal.size - tree.right.x, Fractal.size - tree.right.y);
+      console.log(Fractal.size - tree.x);
+      console.log(line2);
+      this.drawTree(two, tree.left);
+      this.drawTree(two, tree.right);
     }
   }
 };
+
+
+$(document).ready(function(){
+  var elem = document.getElementById('fractal-canvas').children[0];
+  var two = new Two({ width: Fractal.size, height: Fractal.size, type: Two.Types.webgl}).appendTo(elem);
+  var tree = Fractal.tree.buildTree(8, 65*Math.PI/180, {x: Fractal.size/2, y: 300, side:300, angle: 0, left: null, right: null} );
+
+  var line = two.makeLine(Fractal.size/2, Fractal.size, Fractal.size/2, Fractal.size-300);
+  Fractal.tree.drawTree(two, tree);
+  two.update();
+
+});
